5419cb94f05a65e9e9d8a8cabbf108005352938c
Voxdan21 <popov.daniil21@gmail.com>
2022-08-31 21:56:29 +0300

    Добавил решение второй  задачи

---
+++ Task_2/Program.cs
@@ -0,0 +1,11 @@
+﻿// Сбор данных из консоли пользователя
+Console.Write("Введите первое число: ");
+int number_1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int number_2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int number_3 = int.Parse(Console.ReadLine());
+// Поиск максимального числа
+int max = Math.Max(number_3, Math.Max(number_1, number_2));
+// Вывод в консоли максимального числа
+Console.WriteLine($"Большее число : {max}");
